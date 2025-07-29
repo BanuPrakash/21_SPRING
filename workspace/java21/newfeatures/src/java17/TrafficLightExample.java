d7c2f35fa16e90c9e6487aae9b5cd57844962c33
Banuprakash <banuprakashc@yahoo.co.in>
2025-07-29 10:50:42 +0530

    appCDS

--- workspace/java21/newfeatures/src/java17/TrafficLightExample.java
+++ workspace/java21/newfeatures/src/java17/TrafficLightExample.java
@@ -3,7 +3,10 @@ package java17;
 public class TrafficLightExample {
     public static void main(String[] args) {
         TrafficLight light = new Red();
+        // switch as functional
         String result = switch (light) {
+            // if light instanceof Red --> cast it to Red and assign to r
+            // r is a Red type-casted one
             case Red r -> r.getData();
             case Green g -> g.getData();
             case Yellow y -> y.getData();
