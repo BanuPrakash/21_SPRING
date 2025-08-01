d8546c0b820b4dc9b690ab96de344e2b959bbe32
Banuprakash <banuprakashc@yahoo.co.in>
2025-08-01 12:50:27 +0530

    RESTful

--- workspace/rentalapp/src/main/java/com/adobe/rentalapp/aop/LogAspect.java
+++ workspace/rentalapp/src/main/java/com/adobe/rentalapp/aop/LogAspect.java
@@ -2,7 +2,9 @@ package com.adobe.rentalapp.aop;
 
 import lombok.extern.slf4j.Slf4j;
 import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.After;
+import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.springframework.stereotype.Component;
@@ -25,5 +27,14 @@ public class LogAspect {
     @After("execution(* com.adobe.rentalapp.service.RentalService.*(..))")
     public void logAfter(JoinPoint jp) {
         log.info("*******");
+    }
+
+    @Around("execution(* com.adobe.rentalapp.service.RentalService.*(..))")
+    public Object profile(ProceedingJoinPoint  jp) throws  Throwable{
+        long startTime = System.currentTimeMillis();
+            Object ret = jp.proceed();
+        long endTime = System.currentTimeMillis();
+        log.info("Time : " + (endTime - startTime) + " ms");
+        return  ret;
     }
 }
